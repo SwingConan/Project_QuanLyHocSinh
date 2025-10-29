a3981f011ec3e86ea09078b9f523f79520ab436e
SwingConan <loivale.ag@gmail.com>
2025-10-29 18:50:32 +0700

    Fix search giao vien

--- app/Controllers/cGiaoVien.php
+++ app/Controllers/cGiaoVien.php
@@ -2,26 +2,37 @@
 if (!class_exists('cGiaoVien')) {
     include_once __DIR__ . '/../Models/mGiaoVien.php';
 
-    class cGiaoVien {
-        public function getAllTeachers() {
+    class cGiaoVien
+    {
+        public function getAllTeachers()
+        {
             $m = new mGiaoVien();
             return $m->selectAllTeachersWithSubjects(); // có GROUP_CONCAT dsmon
         }
-        public function getSubjectIdsByTeacher($magv) {
+        public function getSubjectIdsByTeacher($magv)
+        {
             $m = new mGiaoVien();
             return $m->getSubjectIdsByTeacher($magv);
         }
-        public function insertTeacher($hoten,$ngaysinh,$gioitinh,$cmnd,$diachi,$dienthoai,$email,$trinhdo,$trangthai,$monhoc_ids=[]) {
+        public function insertTeacher($hoten, $ngaysinh, $gioitinh, $cmnd, $diachi, $dienthoai, $email, $trinhdo, $trangthai, $monhoc_ids = [])
+        {
             $m = new mGiaoVien();
-            return $m->insertTeacher($hoten,$ngaysinh,$gioitinh,$cmnd,$diachi,$dienthoai,$email,$trinhdo,$trangthai,$monhoc_ids);
+            return $m->insertTeacher($hoten, $ngaysinh, $gioitinh, $cmnd, $diachi, $dienthoai, $email, $trinhdo, $trangthai, $monhoc_ids);
         }
-        public function updateTeacher($magv,$hoten,$ngaysinh,$gioitinh,$cmnd,$diachi,$dienthoai,$email,$trinhdo,$trangthai,$monhoc_ids=[]) {
+        public function updateTeacher($magv, $hoten, $ngaysinh, $gioitinh, $cmnd, $diachi, $dienthoai, $email, $trinhdo, $trangthai, $monhoc_ids = [])
+        {
             $m = new mGiaoVien();
-            return $m->updateTeacher($magv,$hoten,$ngaysinh,$gioitinh,$cmnd,$diachi,$dienthoai,$email,$trinhdo,$trangthai,$monhoc_ids);
+            return $m->updateTeacher($magv, $hoten, $ngaysinh, $gioitinh, $cmnd, $diachi, $dienthoai, $email, $trinhdo, $trangthai, $monhoc_ids);
         }
-        public function deleteTeacher($magv) {
+        public function deleteTeacher($magv)
+        {
             $m = new mGiaoVien();
             return $m->deleteTeacher($magv);
+        }
+        public function getAllTeachersBySearch($kw)
+        {
+            $m = new mGiaoVien();
+            return $m->searchTeachersWithSubjects($kw);
         }
     }
 }
